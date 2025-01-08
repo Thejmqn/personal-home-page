402d4163c49a8027a2eb9be2c9e7ab7947650ba7
TheJmqn <jman4461@gmail.com>
2025-01-07 22:45:40 -0500

    Adapted change

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,7 @@ import adapter from '@sveltejs/adapter-static';
 export default {
   kit: {
     adapter: adapter({
-      // fallback: 'index.html' // Uncomment if using client-side routing
+      fallback: 'index.html' // All unmatched routes will fall back to index.html
     })
   }
 };
