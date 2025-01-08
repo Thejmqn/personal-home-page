6f0db54a28bd6883c3c47071e4d7ae1c634b65f4
TheJmqn <jman4461@gmail.com>
2025-01-07 22:58:19 -0500

    Change to personal page

---
+++ src/routes/important_clips/+page.svelte
@@ -0,0 +1,21 @@
+<h1>Some of my favorite YouTube videos. </h1>
+<h2>Can describe a person based on what they watch</h2>
+<div class="Videos">
+    <div class="Sharks">
+        <h3>Talking about SHARKS!</h3>
+        <p>Yes, the full capitalization and exclamation mark are necessary. A great video by CGP Grey describing an obscure legal battle between a liberatarian organization and a London borough. A random ten minute talk about two groups who don't even know the video is being made about them - exactly what the Internet is all about.</p>
+        <iframe width="1280" height="720" src="https://www.youtube.com/embed/_8xhdL8BPvU" title="The Battle of SHARKS!" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    </div>
+    <div class="MedievalEurope">
+        <h3>A Tourist's Guide to Medieval Europe</h3>
+        <p>A really clever history video "disguised" as a guide for time travellers headed to medieval Europe. The history proffesors goes very in-depth on the aspects of everyday Medeival life and explains what it was actually like for these people. More of a podcast.</p>
+        <iframe width="1280" height="720" src="https://www.youtube.com/embed/-aSdFrPnlRg" title="Advice for time traveling to medieval Europe" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    </div>
+    <div class="GameTheory">
+        <h3>Game Theory in the Everyday</h3>
+        <iframe width="1280" height="720" src="https://www.youtube.com/embed/mScpHTIi-kM" title="What Game Theory Reveals About Life, The Universe, and Everything" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    </div>
+    <div class="HelloWorld">
+        <h3>Polyglot Hello World Program</h3>
+        <iframe width="1280" height="720" src="https://www.youtube.com/embed/2L6EE6ZgURE" title="What&#39;s Wrong With This Hello World?" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>    </div>
+</div>
--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,7 @@ import adapter from '@sveltejs/adapter-static';
 export default {
   kit: {
     adapter: adapter({
-      fallback: 'index.html' // All unmatched routes will fall back to index.html
+      fallback: 'app.html' // All unmatched routes will fall back to index.html
     })
   }
 };
