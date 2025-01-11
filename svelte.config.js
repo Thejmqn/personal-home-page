6b969048a3d600eac35ae7e1ce49cfa7d74ad63d
TheJmqn <jman4461@gmail.com>
2025-01-11 11:26:29 -0500

    Revert "Change to personal page"
    This reverts commit 6f0db54a28bd6883c3c47071e4d7ae1c634b65f4.

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,7 @@ import adapter from '@sveltejs/adapter-static';
 export default {
   kit: {
     adapter: adapter({
-      fallback: 'app.html' // All unmatched routes will fall back to index.html
+      fallback: 'index.html' // All unmatched routes will fall back to index.html
     })
   }
 };
